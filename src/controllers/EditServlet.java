2f66dbade01f42d72e725914eb4502a96758daf7
taichi.yoshimura <gobou360@icloud.com>
2021-05-15 19:45:55 +0900

    me

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -15,18 +14,17 @@ import models.Kadai;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class EditServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public EditServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
@@ -35,13 +33,19 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Kadai> messages = em.createNamedQuery("getAllTasks", Kadai.class).getResultList();
+        // 該当のIDのメッセージ1件のみをデータベースから取得
+        Kadai m = em.find(Kadai.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        request.setAttribute("messages", messages);
+        // メッセージ情報とセッションIDをリクエストスコープに登録
+        request.setAttribute("message", m);
+        request.setAttribute("_token", request.getSession().getId());
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+        // メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("message_id", m.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
     }
 }
--- src/models/Kadai.java
+++ src/models/Kadai.java
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 @Entity
 @NamedQueries({
     @NamedQuery(
-        name = "getAllMessages",
+        name = "getAllTasks",
         query = "SELECT m FROM Kadai AS m ORDER BY m.id DESC"
     )
 })
